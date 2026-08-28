4fa978f90ea4720a0aacbccf277bdaba484c081c
Mwaguta <mwatsenzemwaguta14@gmail.com>
2026-08-28 13:15:07 +0300

    views issue rectify

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -322,7 +322,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
     {
         "NAME": (
-            "django.contrinb.auth.password_validation."
+            "django.contrib.auth.password_validation."
             "NumericPasswordValidator"
         ),
     },
